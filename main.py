fc48a0b5e40d6fede9fd938476888aa44884bc69
lactua <minedrayxio@gmail.com>
2024-04-15 17:58:57 +0200

    init commit

---
+++ main.py
@@ -0,0 +1,1 @@
+from src.mods.xor import encrypt, decrypt
---
+++ src/mods/xor/main.py
@@ -0,0 +1,25 @@
+from random import seed, randint
+from hashlib import sha256
+
+def getCryptionKey(key: bytes, length: int) -> bytes:
+    hashed_key = sha256(key).hexdigest().encode()
+
+    seed(sum(hashed_key))
+
+    cryptionKey = bytes(randint(0,255) for _ in range(length))
+
+    return cryptionKey
+
+def encrypt(key: bytes, content: bytes) -> bytes:
+    cryption_key = getCryptionKey(key, len(content))
+
+    encrypted_content = bytes([c ^ k for c, k in zip(content, cryption_key)])
+
+    return encrypted_content
+
+def decrypt(key: bytes, encrypted_content: bytes) -> bytes:
+    cryption_key = getCryptionKey(key, len(encrypted_content))
+
+    content = bytes([e ^ k for e, k in zip(encrypted_content, cryption_key)])
+
+    return content
